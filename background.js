9fb05052661a6e127425239410dc60127ed9a339
Limmy <limych24@gmail.com>
2020-01-07 18:04:43 +0200

    0.00.0003: Add stub for script running only on needed page

--- background.js
+++ background.js
@@ -1,7 +1,22 @@
+/*chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+
+    var tablink = tab.url;
+
+        var url = new URL(tablink);
+        if (url.hostname === "twitter.com") {
+            //Do work
+            //var gList = document.getElementsByTagName("g");
+            console.log("loaded");
+            window.alert(document.body.innerText.length);
+            
+        }
+
+  }
+})*/
 chrome.runtime.onInstalled.addListener(function() {
         //need?
   });
-  
  chrome.tabs.onUpdated.addListener(function() {
     chrome.tabs.getSelected(null,function(tab) {
     var tablink = tab.url;
---
+++ myscript.js
@@ -0,0 +1,8 @@
+// Checking page title
+
+    //Creating Elements
+    var btn = document.createElement("BUTTON")
+    var t = document.createTextNode("CLICK ME");
+    btn.appendChild(t);
+    //Prepending to DOM 
+    document.body.prepend(btn);
