0112032c70157e61423f9f72b4ba6c2f4a35ebfe
Limmy <limych24@gmail.com>
2020-01-12 16:35:26 +0200

    0.00.0003: Hide RTs stub

--- myscript.js
+++ myscript.js
@@ -6,3 +6,37 @@
     btn.appendChild(t);
     //Prepending to DOM 
     document.body.prepend(btn);
+    var isBindingActive = false;
+    addObserver();
+
+
+function addObserver() {
+    console.log("Adding observer");
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    var observer = new MutationObserver(function(mutations, observer) {
+            if (!isBindingActive) {
+                isBindingActive = true;
+                hideRTs("r-111h2gw r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1xzupcd"); //TODO detect className
+                isBindingActive = false;
+            }
+    });
+    
+    observer.observe(document, {
+            subtree:true,
+            attributes:false,
+            childList: true,
+            characterData: true
+            //...
+    });
+}
+    //
+
+function hideRTs(rtClassName) {
+    var allElementsWithRtClassName = document.getElementsByClassName(rtClassName);
+    
+    console.log("Elements with " + rtClassName + " " + allElementsWithRtClassName.length);
+    
+     for (const elementToHide of allElementsWithRtClassName) {
+        elementToHide.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.setAttribute("style", "display:none");
+    }
+}
