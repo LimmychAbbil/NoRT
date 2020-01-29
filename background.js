f3253c3f2b2438c8ba862536c93f091574ee1893
Limmy <limych24@gmail.com>
2020-01-29 21:51:42 +0200

    0.01.0001: Remove testing files
    * Start implementing extention popup

--- background.js
+++ background.js
@@ -17,14 +17,3 @@
 chrome.runtime.onInstalled.addListener(function() {
         //need?
   });
- chrome.tabs.onUpdated.addListener(function() {
-    chrome.tabs.getSelected(null,function(tab) {
-    var tablink = tab.url;
-
-    var url = new URL(tablink);
-    if (url.hostname === "twitter.com") {
-        var gList = document.getElementsByTagName("g");
-        window.alert(gList.length);
-    }
-});
- });
--- popup.js
+++ popup.js
@@ -1,22 +1,4 @@
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  if (request.action == "getSource") {
-    message.innerText = request.source;
-  }
-});
-
-function onWindowLoad() {
-
-  var message = document.querySelector('#message');
-
-  chrome.tabs.executeScript(null, {
-    file: "getPagesSource.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-    }
-  });
-
+document.getElementById("showRTs").addEventListener("click", handleClick);
+function handleClick() {
+  window.alert("Clicked, new value = " + document.getElementById("showRTs").checked);
 }
-
-window.onload = onWindowLoad;
