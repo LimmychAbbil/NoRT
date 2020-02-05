6798d8eef16049298600d5601993afcf7acb139b
Limmy <limych24@gmail.com>
2020-02-05 23:33:01 +0200

    0.01.0001: Add stub to save config in chrome storage

--- popup.js
+++ popup.js
@@ -1,4 +1,21 @@
 document.getElementById("showRTs").addEventListener("click", handleClick);
 function handleClick() {
-  window.alert("Clicked, new value = " + document.getElementById("showRTs").checked);
+    chrome.storage.sync.set({ mytext: document.getElementById("showRTs").checked });
+    
+    //reload page
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.reload(tabs[0].id);
+    });
+    
+    chrome.storage.sync.get(['mytext'], function(result) {
+          window.alert('Value currently is ' + result.mytext);
+        });
 }
+/*LISTENER FOR DEBUG PURPOSES:
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+        for (var key in changes) {
+          var storageChange = changes[key];
+          window.alert('Storage key' + key + ' in namespace' + namespace +' changed. ' +
+                      'Old value was ' + storageChange.oldValue + ', new value is ' + storageChange.newValue);
+        }
+      });*/ 
